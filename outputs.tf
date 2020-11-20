e617620142ab649f002e90ccf887a45ec55fbffb
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2020-11-20 13:02:38 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output primary_network_interface_id {
 value = ibm_is_instance.instance.primary_network_interface[0].id
 }
+
+output ips {
+  value = ibm_is_instance.instance.primary_network_interface[*].id
+  }
