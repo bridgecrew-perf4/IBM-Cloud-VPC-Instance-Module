0567f578227e675e6308e4d5840345a108ef12f6
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-06-09 16:25:55 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-variable subnet_id {}
-variable name {}
-variable vpc_id {}
-variable resource_group {}
+variable "subnet_id" {}
+variable "name" {}
+variable "vpc_id" {}
+variable "resource_group" {}
 
-variable ssh_keys {}
-variable image_name {
+variable "ssh_keys" {}
+variable "image_name" {
   default = "ibm-ubuntu-20-04-minimal-amd64-2"
 }
-variable profile_name {
+variable "profile_name" {
   default = "cx2-2x4"
 }
-variable tags {}
-variable security_group_id {}
-variable zone {}
-variable user_data {}
+variable "tags" {}
+variable "security_group" {}
+variable "zone" {}
+variable "user_data" {}
