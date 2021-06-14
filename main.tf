256b91b343b23f28e71c0386ff3db010d172347e
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-06-14 09:40:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "ibm_is_instance" "instance" {
   image          = data.ibm_is_image.image.id
   keys           = var.ssh_keys
   resource_group = var.resource_group
-  allow_ip_spoofing = var.allow_ip_spoofing != "" ? var.allow_ip_spoofing : false
+  
 
   user_data = var.user_data != "" ? var.user_data : file("${path.module}/init.yml")
   
@@ -14,6 +14,7 @@ resource "ibm_is_instance" "instance" {
   primary_network_interface {
     subnet          = var.subnet_id
     security_groups = [var.security_groups != null ? var.security_groups : null]
+    allow_ip_spoofing = var.allow_ip_spoofing != "" ? var.allow_ip_spoofing : false
   }
 
   boot_volume {
